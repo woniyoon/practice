e76122a60817dede8104a29204abc28f4ab37613
woniyoon <gellifisch@gmail.com>
2020-09-19 18:57:30 +0900

    Remove unused component

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import Navigator from './components/Navigator';
 import Main from './routes/Main';
+import About from './routes/About';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Navigator />
     <Route path="/" exact={true} component={Main} />
+    <Route path="/about" component={About} />
   </BrowserRouter>,
   document.getElementById('root')
 );
