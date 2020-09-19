bae98d9015667b3da79fc46010d08d9cf85a02d1
woniyoon <gellifisch@gmail.com>
2020-09-19 19:30:16 +0900

    Add route for Works page

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import './index.css';
 import Navigator from './components/Navigator';
 import Main from './routes/Main';
 import About from './routes/About';
+import Works from './routes/Works';
 
 ReactDOM.render(
   <BrowserRouter>
     <Navigator />
     <Route path="/" exact={true} component={Main} />
     <Route path="/about" component={About} />
+    <Route path="/works" component={Works} />
   </BrowserRouter>,
   document.getElementById('root')
 );
