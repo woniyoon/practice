b79ae005c4c4ec4990c03a2cf654875b572458fb
woniyoon <gellifisch@gmail.com>
2020-09-19 20:14:59 +0900

    Change Project to class component

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import './Project.css';
 
-function Project(props){
-    console.log(props.work);
-    const work = props.work;
-    return(
-        <div className="projectPcs">
-            <h5>{work.title}</h5>
-            <h5>{work.period}</h5>
-        </div>
-    );
+class Project extends React.Component {
+
+    test = () => {
+        console.log("요호~ ");
+    }
+
+    render(){
+        const work = this.props.work;
+
+        var divStyle = {
+            backgroundImage: "url(" + work.projectThumnail + ")"
+        }
+
+        return(
+            <div className="projectPcs" style={divStyle} onMouseEnter={this.test} >
+                {/* <img className="projectThumnail" src={work.projectThumnail}/> */}
+                <p className="projectTitle">{work.title}</p>
+                <p className="projectPeriod">{work.period}</p>
+            </div>
+        );
+    }
 }
 
 export default Project;
