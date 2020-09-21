32699b3ac6801eb5cd9bb983b8cf0cf91961255c
woniyoon <gellifisch@gmail.com>
2020-09-21 21:26:25 +0900

    Style navigator component

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -5,17 +5,20 @@ import profile from '../images/profile.jpg';
 
 function Navigator(){
     return (
-        <aside className="sideBar">
+        <aside className="sideBar" align="center">
             <img className="sideImg" src={profile} alt="img" />
-            <ul className="navMenu">
+            <ul className="navMenu" >
                 <li>
                     <Link to="/">MAIN</Link>
                 </li>
-                <li>
+                {/* <li>
                     <Link to="/about">ABOUT</Link>
-                </li>
+                </li> */}
                 <li>
                     <Link to="/works">WORKS</Link>
+                </li>
+                <li>
+                    <a href="https://woniyoon.github.io/">BLOG</a>
                 </li>
             </ul>
             <p>ENG/KOR</p>
