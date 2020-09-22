ae822a7347b66d91908d304aaa1ad2da8cf55704
woniyoon <gellifisch@gmail.com>
2020-09-22 18:30:31 +0900

    Improve modal view css

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './Modal.css';
 
 function Modal({ close, project }){
 
     console.log(close);
     return (
-        <div className="modal">Modal!!!</div>
+        <div className="modalContainer">
+            <div className="modalOverlay">
+            </div>
+            <main className="modalContent">
+                <p onClick={close}>X</p>
+                <div className="modal">Modal!!!</div>
+            </main>
+        </div>
     );
 }
 
