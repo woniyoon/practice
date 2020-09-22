500d19ceb08741611938c4821580da6ca934b30f
woniyoon <gellifisch@gmail.com>
2020-09-22 15:24:21 +0900

    Create Modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Modal({ close, project }){
+
+    console.log(close);
+    return (
+        <div className="modal">Modal!!!</div>
+    );
+}
+
+export default Modal;
