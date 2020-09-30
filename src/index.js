80a929eabcdc6382332ecfc13f216db2a68681df
woniyoon <gellifisch@gmail.com>
2020-09-30 14:19:04 +0900

    Replace BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import './index.css';
 import Navigator from './components/Navigator';
 import Main from './routes/Main';
@@ -8,11 +8,11 @@ import About from './routes/About';
 import Works from './routes/Works';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Navigator />
     <Route path="/" exact={true} component={Main} />
     <Route path="/about" component={About} />
     <Route path="/works" component={Works} />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
