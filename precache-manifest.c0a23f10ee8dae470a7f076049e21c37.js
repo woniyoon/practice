fadeffd880546162091618f94b8e5877fd9a8cd9
woniyoon <gellifisch@gmail.com>
2020-09-30 14:24:43 +0900

    Updates

---
+++ precache-manifest.c0a23f10ee8dae470a7f076049e21c37.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479ae77bc256f8165e8a0eded8ca4e13",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3c237e371a596a4e1e01",
+    "url": "/portfolio/static/css/main.8c7c21f5.chunk.css"
+  },
+  {
+    "revision": "d9be033384f309d03c2d",
+    "url": "/portfolio/static/js/2.68f020a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.68f020a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c237e371a596a4e1e01",
+    "url": "/portfolio/static/js/main.c7c98fbc.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f230b5d939fd756e255783ed9a0f59f2",
+    "url": "/portfolio/static/media/profile.f230b5d9.jpg"
+  },
+  {
+    "revision": "2d689a81f3162495756305ff6a2e4f45",
+    "url": "/portfolio/static/media/sbArch.2d689a81.jpeg"
+  },
+  {
+    "revision": "3b856ae4b1c7e4a2de685f2c84048396",
+    "url": "/portfolio/static/media/sbWeb.3b856ae4.png"
+  },
+  {
+    "revision": "939567e5438076fe4889f7b952c65c45",
+    "url": "/portfolio/static/media/sdArch.939567e5.jpeg"
+  },
+  {
+    "revision": "df1b81a9a9ba387f6de0a5b70bb1d61d",
+    "url": "/portfolio/static/media/socdocLogoT.df1b81a9.png"
+  }
+]);
